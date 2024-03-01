1cedda31186904c6f1dfc3dbb67d11bc293b5fc3
maxbartrip <maximillianbartrip@yahoo.co.uk>
2024-03-01 02:23:48 +0000

    Initial Commit

---
+++ src/main/java/mtkb/disaster/disasterplugin/DisasterPlugin.java
@@ -0,0 +1,17 @@
+package mtkb.disaster.disasterplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DisasterPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
