c23fa5c323020d19775e4e3b62e09fb18030cc88
maxbartrip <maximillianbartrip@yahoo.co.uk>
2024-03-03 02:23:33 +0000

    Added functionality of potion effect disaster as well as force disaster command for testing purposes

--- src/main/java/mtkb/disaster/disasterplugin/commands/DisasterCommand.java
+++ src/main/java/mtkb/disaster/disasterplugin/commands/DisasterCommand.java
@@ -21,9 +21,7 @@ public class DisasterCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        //sender.sendMessage("We in here");
             if(args[0].equalsIgnoreCase("enable")) {
-                //sender.sendMessage("We in enabled");
                 // Enable disaster plugin here
                 if (plugin.isDisasterEnabled()) {
                     sender.sendMessage("Disaster plugin is already enabled.");
@@ -36,7 +34,6 @@ public class DisasterCommand implements CommandExecutor, TabCompleter {
                 return true;
             }
             else if (args[0].equalsIgnoreCase("disable")) {
-                //sender.sendMessage("We in disabled");
                 if(plugin.isDisasterEnabled()){
                     plugin.setDisasterEnabled(false);
                     Bukkit.broadcast(new TextComponent("Disaster plugin disabled"));
@@ -47,7 +44,65 @@ public class DisasterCommand implements CommandExecutor, TabCompleter {
                 }
                 return true;
             }
+            else if (args[0].equalsIgnoreCase("force")) {
+                if(args.length>=2) {
+                    String disasterType = args[1].toLowerCase();
+                    switch (disasterType) {
+                        case "potion":
+                            if (args.length>=3) {
+                                int effectId = getEffectId(args[2].toLowerCase());
+                                DisasterManager.potionEffectDisaster(effectId);
+                            }
+                            else {
+                                DisasterManager.forceDisaster(0);
+                            }
+                            break;
+                        default:
+                            sender.sendMessage("§cDisaster type not recognized.");
+                            break;
+                    }
+                }
+                return true;
+            }
         return false;
+    }
+
+    public static int getEffectId(String effectName) {
+        switch (effectName) {
+            // Blindness 0
+            // Darkness 1
+            // Hunger 2
+            // Levitation 3
+            // Mining fatigue 4
+            // Nausea 5
+            // Poison 6
+            // Slowness 7
+            // Weakness 8
+            // Wither 9
+            case "blindness":
+                return 0;
+            case "darkness":
+                return 1;
+            case "hunger":
+                return 2;
+            case "levitation":
+                return 3;
+            case "fatigue":
+                return 4;
+            case "nausea":
+                return 5;
+            case "poison":
+                return 6;
+            case "slowness":
+                return 7;
+            case "weakness":
+                return 8;
+            case "wither":
+                return 9;
+            default:
+                //Bukkit.broadcastMessage("Unknown effect ID");
+                return 10;
+        }
     }
 
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String [] args) {
@@ -56,6 +111,26 @@ public class DisasterCommand implements CommandExecutor, TabCompleter {
         if (args.length==1){
             completions.add("enable");
             completions.add("disable");
+            completions.add("force");
+        }
+        else if (args.length==2) {
+            if (args[0].equalsIgnoreCase("force")) {
+                completions.add("potion");
+            }
+        }
+        else if (args.length==3) {
+            if (args[0].equalsIgnoreCase("force") && args[1].equalsIgnoreCase("potion")){
+                completions.add("blindness");
+                completions.add("darkness");
+                completions.add("hunger");
+                completions.add("levitation");
+                completions.add("fatigue");
+                completions.add("nausea");
+                completions.add("poison");
+                completions.add("slowness");
+                completions.add("weakness");
+                completions.add("wither");
+            }
         }
         return completions;
     }
--- src/main/java/mtkb/disaster/disasterplugin/commands/DisasterManager.java
+++ src/main/java/mtkb/disaster/disasterplugin/commands/DisasterManager.java
@@ -2,8 +2,10 @@ package mtkb.disaster.disasterplugin.commands;
 
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitScheduler;
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +22,9 @@ public class DisasterManager {
         this.scheduler = scheduler;
         this.time = time;
         this.disasterList = new ArrayList<>();
-        disasterList.add(this::testDisaster);
+        //disasterList.add(this::testDisaster); How to add disasters to list
+        //disasterList.add(()-> exampleDisaster(x)); How to add disaster which takes a parameter
+        disasterList.add(()-> potionEffectDisaster(-1));
     }
 
     public static void performDisaster() {
@@ -28,7 +32,73 @@ public class DisasterManager {
         disasterList.get(randomNumber).run();
     }
 
-    private void testDisaster() {
-        Bukkit.broadcast(new TextComponent("DISASTER AHH"));
+    public static void forceDisaster(int disasterIndex) {
+        disasterList.get(disasterIndex).run();
+    }
+
+    public static void potionEffectDisaster(int givenId) {
+
+        // List of chosen potion effects and their Id
+        // Blindness 0
+        // Darkness 1
+        // Hunger 2
+        // Levitation 3
+        // Mining fatigue 4
+        // Nausea 5
+        // Poison 6
+        // Slowness 7
+        // Weakness 8
+        // Wither 9
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            Bukkit.broadcast(new TextComponent("§aDISASTER: RANDOM POTION EFFECT"));
+            int duration = 15 * 20;
+            int effectId = random.nextInt(10);
+            if (givenId != -1) {
+                effectId = givenId;
+            }
+
+            PotionEffect potionEffect = null;
+
+            switch (effectId) {
+                case 0:
+                    potionEffect = new PotionEffect(PotionEffectType.BLINDNESS, duration, 0);
+                    break;
+                case 1:
+                    potionEffect = new PotionEffect(PotionEffectType.DARKNESS, duration, 0);
+                    break;
+                case 2:
+                    potionEffect = new PotionEffect(PotionEffectType.HUNGER, duration, 0);
+                    break;
+                case 3:
+                    potionEffect = new PotionEffect(PotionEffectType.LEVITATION, duration, 0);
+                    break;
+                case 4:
+                    potionEffect = new PotionEffect(PotionEffectType.SLOW_DIGGING, duration, 0);
+                    break;
+                case 5:
+                    potionEffect = new PotionEffect(PotionEffectType.CONFUSION, duration, 0);
+                    break;
+                case 6:
+                    potionEffect = new PotionEffect(PotionEffectType.POISON, duration, 0);
+                    break;
+                case 7:
+                    potionEffect = new PotionEffect(PotionEffectType.SLOW, duration, 0);
+                    break;
+                case 8:
+                    potionEffect = new PotionEffect(PotionEffectType.WEAKNESS, duration, 0);
+                    break;
+                case 9:
+                    potionEffect = new PotionEffect(PotionEffectType.WITHER, duration, 0);
+                    break;
+                default:
+                    Bukkit.broadcast(new TextComponent("§cUnknown effect ID"));
+                    break;
+            }
+
+            if (potionEffect != null) {
+                player.addPotionEffect(potionEffect);
+            }
+        }
     }
 }
